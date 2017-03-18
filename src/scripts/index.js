4cb9b0c57fa07d1d723de8ac219d96be872b2bb1
Brandon Pereira <brandonp@criticalmass.com>
2017-03-18 15:25:10 -0600

    animate height

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,9 @@
 	
 	// Function to bootstrap all page components
 	$(window).on('ready', function() {
-		// new Skills($('#skills'));		
-		// new Projects($('#projects'));		
-		// new Contact($('#contact'));		
+		new Skills($('#skills'));		
+		new Projects($('#projects'));		
+		new Contact($('#contact'));		
 		new Apps($('#apps'));		
 		new SmoothScroll().events($('section.header'));
 		
