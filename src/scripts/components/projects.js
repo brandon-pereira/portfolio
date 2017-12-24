546c2bf2c19545b3dc6d9ddcb841741bcab6332a
Brandon Pereira <brandonp@criticalmass.com>
2017-12-24 09:37:26 -0700

    work to projects redesign

--- src/scripts/components/projects.js
+++ src/scripts/components/projects.js
@@ -3,28 +3,74 @@ import Base from './base';
 export default class Projects extends Base {
 
     init() {
-        this.container = this.el.querySelector(".projects");
+        this.container = this.el.querySelector('.projects');
+        this.currentlyVisibleProjects = this.container.children.length;
         this.loadMore = this.el.querySelector('.loadMore');
-        this.salvattore = import('salvattore');
+        this.skeleton = this.el.querySelector('.project.skeleton');
+        this.salvattore = import('salvattore'); // TODO: Salvattore auto-initializes when loaded, we should use a different module.
+        this._projects = null // Projects cache
         return super.init();
     }
 
     events() {
-        this.salvattore.then(salvattore => {
-            this.loadMore.addEventListener("click", () => {
-                this.loadMore.classList.add("loading");
-                const newEl = document.createElement("div");
-                salvattore.appendElements(this.container, newEl);
-                console.log(newEl);
-                this.loadMore.classList.remove("loading");
-            });
-
-              window.addEventListener("resize", () => {
-                this.salvattore.then(salvattore => {
-                  salvattore.recreateColumns(this.container);
-                });
-              });
+        this.loadMore.addEventListener('click', () => {
+            this.loadMore.classList.add("loading");
+            this.showMoreProjects()
+                .then(() => setTimeout(() => this.loadMore.classList.remove("loading"), 500));
         });
+
+        this.salvattore.then(salvattore =>
+              window.addEventListener("resize", () => // TODO: Debounce
+                  salvattore.recreateColumns(this.container)
+              )
+        );
+    }
+
+    showMoreProjects() {
+        return Promise.all([this.fetchProjects(), this.salvattore])
+            .then(([projects, salvattore]) => {
+                const toAdd = projects.projects.slice(this.currentlyVisibleProjects, this.currentlyVisibleProjects + 3);
+                this.currentlyVisibleProjects += 3;
+                if(this.currentlyVisibleProjects >= projects.projects.length) {
+                    console.log("the end");
+                    this.loadMore.classList.add('hidden');
+                } else {
+                    this.loadMore.classList.remove('hidden');
+                }
+                return [toAdd, salvattore];
+            })
+            .then(([toAdd, salvattore]) => {
+                return [toAdd.map((project) => this.createProjectNode(project)), salvattore];
+            })
+            .then(([els, salvattore]) => this.addElementsToGrid(salvattore, this.container, els))
+            // .then((projects) => this.addElementsToGrid())
+    }
+
+    createProjectNode(project) {
+        const $project = this.skeleton.cloneNode(true);
+        $project.classList.remove('hidden', 'skeleton');
+        $project.querySelector('[data-project-title]').innerText = project.title;
+        $project.querySelector('[data-project-description]').innerHTML = project.shortDescription || project.description;
+        if(project.images && project.images.length) {
+            $project.querySelector("img").src = project.images[0].src;
+        }
+        return $project;
+    }
+
+    addElementsToGrid(salvattore, grid, elements) {
+        salvattore.appendElements(grid, elements);
+    }
+
+    fetchProjects() {
+        if(this._projects) {
+            return Promise.resolve(this._projects);
+        } else {
+            return import('../../content/projects.json')
+                .then((projects) => {
+                    this._projects = projects;
+                    return this._projects;
+                })
+        }
     }
 
 
