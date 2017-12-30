f2d39a967cb0d5d74bc04bb2a5920cbf38da3f03
Brandon Pereira <brandonp@criticalmass.com>
2017-12-30 15:03:09 -0700

    improving runtime

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,22 @@
-if('animate' in document.body === false) {
-    import("web-animations-js");
-}
-
 import("../styles/style.scss");
 import("./components/webfontloader").then(webfontloader => webfontloader());
-import("./components/lightbox");
+
+// Async load all components
 const components = [
     [import("./components/skills"), document.querySelector(".skills"), {}],
     [import("./components/apps"), document.querySelector(".apps"), {}],
     [import("./components/projects"), document.querySelector(".projects"), {}]
 ];
-
 components.forEach(component => {
+    // Components export a class, so we instantiate
     component[0].then(Class => new Class(component[1], component[2] || {}));
 });
+
+
+// Async load animation polyfill if not supported (not needed till user interaction)
+if ('animate' in document.body === false) {
+    import("web-animations-js");
+}
 
 // If its a webkit browser add 'webkit' class to HTML.
 if ("webkitTextFillColor" in document.documentElement.style) {
