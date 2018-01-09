ee456746bc64cf1b69cd9d2600d18962ba3e25b6
Brandon Pereira <brandonp@criticalmass.com>
2018-01-08 19:46:05 -0700

    ga export

--- src/scripts/services/analytics.js
+++ src/scripts/services/analytics.js
@@ -1,9 +1,14 @@
-class Analytics {
-
-    send() {
-        console.log("Analytics: TODO")
+/**
+ * Safely fire GA Event
+ * See: https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+ * @param {String} category
+ * @param {String} action
+ * @param {String} label
+ */
+export default function sendEvent(category, action, label) {
+    if (window.ga) {
+        window.ga('send', 'event', category, action, label);
+    } else {
+        console.warn("Google Analytics not detected on page. Might be blocked?");
     }
-
 }
-
-export default new Analytics();
