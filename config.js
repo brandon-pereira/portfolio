35ecdc031ab14d12bab47afce80afc772a583435
Brandon Pereira <brandonp@criticalmass.com>
2018-07-21 22:24:20 -0600

    auto reload js too

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
 		},
 		dist: './dist',
 		watch: {
-			scripts: ['./src/scripts/**/*.js', './src/styles/**/*.scss'],
+			scripts: ['./src/scripts/**/*.js', './src/styles/**/*.scss', './src/content/*.json'],
 			html: ['./src/html/**/*.html', './src/styles/critical.css', './src/content/*.json'],
 			svg: './src/svgs/*.svg',
 			static: './src/static/**/*'
