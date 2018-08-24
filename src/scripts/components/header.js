48b1529879e9eb90f7da58e8d65022e0d69d9008
Brandon Pereira <brandonp@criticalmass.com>
2018-08-24 16:22:26 -0600

    reverted all that blob stuff because of bad browser performance

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -2,37 +2,8 @@ import Base from './base';
 
 export default class Skills extends Base {
   init() {
-    this.letterize(this.el.querySelector('h1'));
-    return super.init(import('../../styles/header.scss')).then(() => {
-      this.blobs = this.generateBlobs(this.props.numberOfBlobs);
-      this.randomizeBlobCoordinates(this.blobs);
-      setInterval(() => {
-        requestAnimationFrame(() => this.randomizeBlobCoordinates(this.blobs));
-      }, 5000);
-    });
-  }
-
-  generateBlobs(numberToGenerate = 30) {
-    const bg = this.el.querySelector('.background');
-    const blob = bg.querySelector('.blob');
-    for (let i = 0; i < numberToGenerate; i++) {
-      const _blob = blob.cloneNode();
-      blob.style.transform = this.getTranslateString(this.width, this.height);
-      bg.appendChild(_blob);
-    }
-    return bg.querySelectorAll('.blob');
-  }
-
-  randomizeBlobCoordinates(blobs) {
-    blobs.forEach(blob => {
-      blob.style.transform = this.getTranslateString(this.width, this.height);
-    });
-  }
-
-  getTranslateString(maxX, maxY) {
-    const x = this._randomNumber(0, maxX);
-    const y = this._randomNumber(0, maxY);
-    return `translate(${x}px, ${y}px)`;
+    this.letterize(this.el.querySelector('h1.name'));
+    return super.init(import('../../styles/header.scss'));
   }
 
   letterize(el) {
@@ -49,17 +20,5 @@ export default class Skills extends Base {
       nodes.forEach(node => el.appendChild(node));
     }
     return el;
-  }
-
-  _randomNumber(min, max) {
-    return Math.floor(Math.random() * max) + min;
-  }
-
-  get height() {
-    return this.el.clientHeight;
-  }
-
-  get width() {
-    return this.el.clientWidth;
   }
 }
