fb5c610bb46b42efe73f629c49c2b1d53d694a3e
Brandon Pereira <brandonp@criticalmass.com>
2018-08-29 15:17:47 -0600

    removed target blank

--- content/utils/md2html.mjs
+++ content/utils/md2html.mjs
@@ -1,19 +1,19 @@
 import marked from 'marked';
 
 //https://github.com/markedjs/marked/issues/655#issuecomment-383226346
-const renderer = new marked.Renderer();
-const linkRenderer = renderer.link;
-renderer.link = (href, title, text) => {
-  const html = linkRenderer.call(renderer, href, title, text);
-  return html.replace(/^<a /, '<a target="_blank" ');
-};
+// const renderer = new marked.Renderer();
+// const linkRenderer = renderer.link;
+// renderer.link = (href, title, text) => {
+//   const html = linkRenderer.call(renderer, href, title, text);
+//   return html.replace(/^<a /, '<a target="_blank" ');
+// };
 
 export default md =>
   new Promise((resolve, reject) => {
     if (!md) {
       return '';
     }
-    marked(md, { renderer }, (err, html) => {
+    marked(md, {}, (err, html) => {
       err ? reject(err) : resolve(html);
     });
   });
