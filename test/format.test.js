69d571b6136741c4e951110f0277eef78cca7794
Brandon Pereira <brandonp@criticalmass.com>
2018-08-29 10:56:59 -0600

    fixed unit tests/linted data

--- test/format.test.js
+++ test/format.test.js
@@ -1,45 +1,50 @@
-const projects = require('../src/content/projects.json');
-const skills = require('../src/content/skills.json');
+require('jest-matcher-one-of');
+const projects = require('../content/data/projects.json');
+const skills = require('../content/data/skills.json');
 
 describe('Formatting - Projects', () => {
-    projects.projects.forEach((el, i) => {
-        test(el.title || i, () => {
-            expect(typeof el.title).toBe('string'); // Title
-            expect(Array.isArray(el.languages)).toBeTruthy(); // Languages
-            expect(!isNaN(new Date(el.date))).toBeTruthy(); // Valid Date
-            expect(el.description).toBeDefined(); // Description
-            expect(projects.types[el.type]).toBeDefined(); // Type
-            expect(Array.isArray(el.images)).toBeTruthy(); // Images if array
-            if(el.gitUrl) {
-                expect(el.gitUrl.startsWith('http')).toBeTruthy()
-            }
-            el.images.forEach((img) => { // Image sanity checks
-                expect(typeof img.src).toBe('string');
-                expect(typeof img.title).toBe('string');
-                expect(typeof img.preview).toBe('string');
-            });
-            expect(typeof projects.statuses[el.status]).toBe('object'); // Check valid status
-            expect(typeof el.link).toBe('string'); // Check valid link
-        });
+  projects.forEach((el, i) => {
+    test(el.title || i, () => {
+      expect(typeof el.title).toBe('string'); // Title
+      expect(Array.isArray(el.languages)).toBeTruthy(); // Languages
+      expect(!isNaN(new Date(el.date))).toBeTruthy(); // Valid Date
+      expect(el.description).toBeDefined(); // Description
+      expect(el.type).toBeOneOf(['External', 'Internal']); // Type
+      expect(Array.isArray(el.images)).toBeTruthy(); // Images if array
+      if (el.gitUrl) {
+        expect(el.gitUrl.startsWith('http')).toBeTruthy();
+      }
+      el.images.forEach(img => {
+        // Image sanity checks
+        expect(typeof img.url).toBe('string');
+        expect(typeof img.title).toBe('string');
+      });
+      expect(el.status).toBeOneOf([
+        'Live',
+        'Beta',
+        'Coming Soon',
+        'Unavailable'
+      ]); // Check valid status
+      // expect(typeof el.link).toBe('string'); // Check valid link
     });
+  });
 });
 
 describe('Formatting - Skills', () => {
-    skills.categories.forEach((category, i) => {
-        describe(category.name || i, () => {
-            test('Structure', () => {
-                expect(typeof category.name).toBe('string');
-                expect(Array.isArray(category.skills)).toBeTruthy();
-            })
-            category.skills.forEach((skill, i) => {
-                test(skill.name || i, () => {
-                    expect(typeof skill.id).toBe('string');
-                    expect(typeof skill.name).toBe('string');
-                    expect(typeof skill.description).toBe('string');
-                    expect(typeof skill.skillLevel).toBe('number');
-
-                })
-            });
+  skills.forEach((category, i) => {
+    describe(category.name || i, () => {
+      test('Structure', () => {
+        expect(typeof category.name).toBe('string');
+        expect(Array.isArray(category.skills)).toBeTruthy();
+      });
+      category.skills.forEach((skill, i) => {
+        test(skill.name || i, () => {
+          expect(typeof skill._id).toBe('string');
+          expect(typeof skill.name).toBe('string');
+          expect(typeof skill.description).toBe('string');
+          expect(skill.skillLevel).toBeOneOf(['Pro', 'Intermediate', 'Novice']);
         });
+      });
     });
+  });
 });
--- test/keys.test.js
+++ test/keys.test.js
@@ -1,40 +1,40 @@
-const projects = require('../src/content/projects.json');
-const skills = require('../src/content/skills.json');
+const projects = require('../content/data/projects.json');
+const skills = require('../content/data/skills.json');
 
-const getAllIdsFromProjects = (projects) => {
-    const ids = [];
-    projects.projects.forEach(project => {
-        ids.push(...project.languages);
-    });
-    return Array.from(new Set(ids));
-}
+const getAllIdsFromProjects = projects => {
+  const ids = [];
+  projects.forEach(project => {
+    ids.push(...project.languages.map(lang => lang._id));
+  });
+  return Array.from(new Set(ids));
+};
 
-const getAllIdsFromSkills = (skills) => {
-    const ids = [];
-    skills.categories.forEach(category => {
-        category.skills.forEach(skill => {
-            ids.push(skill.id);
-        })
+const getAllIdsFromSkills = skills => {
+  const ids = [];
+  skills.forEach(category => {
+    category.skills.forEach(skill => {
+      ids.push(skill._id);
     });
-    return Array.from(new Set(ids));
-}
+  });
+  return Array.from(new Set(ids));
+};
 
 describe('Valid Keys', () => {
-    const projectIds = getAllIdsFromProjects(projects);
-    const skillIds = getAllIdsFromSkills(skills);
-    test('Project IDs', () => {
-        const invalidIds = projectIds.filter((id) => !skillIds.includes(id));
-        if (invalidIds.length) {
-            console.error(invalidIds);
-        }
-        expect(invalidIds.length).toBe(0);
-    });
+  const projectIds = getAllIdsFromProjects(projects);
+  const skillIds = getAllIdsFromSkills(skills);
+  test('Project IDs', () => {
+    const invalidIds = projectIds.filter(id => !skillIds.includes(id));
+    if (invalidIds.length) {
+      console.error(invalidIds);
+    }
+    expect(invalidIds.length).toBe(0);
+  });
 
-    test('Skill IDs', () => {
-        const invalidIds = skillIds.filter((id) => !projectIds.includes(id));
-        if (invalidIds.length) {
-            console.error(invalidIds);
-        }
-        expect(invalidIds.length).toBe(0);
-    });
+  test('Skill IDs', () => {
+    const invalidIds = skillIds.filter(id => !projectIds.includes(id));
+    if (invalidIds.length) {
+      console.error(invalidIds);
+    }
+    expect(invalidIds.length).toBe(0);
+  });
 });
