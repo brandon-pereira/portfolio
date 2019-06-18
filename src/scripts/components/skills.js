1577d6eb28c09038411488aac1ae77fe73bdc508
Brandon Pereira <brandonp@criticalmass.com>
2019-06-17 19:38:25 -0600

    feat(skills): added a11y keyboard suppport

--- src/scripts/components/skills.js
+++ src/scripts/components/skills.js
@@ -41,22 +41,45 @@ export default class Skills extends Base {
 
     // Toggle current element
     el.classList.toggle('open');
+    const isOpen = el.classList.contains('open');
+    this.toggleTabIndex(el, isOpen);
 
     // Animate open if supported
-    if (el.classList.contains('open')) {
+    if (isOpen) {
       const toHeight = el.clientHeight + 'px';
       animate(el, [{ height: 0 }, { height: toHeight }], { duration: 200 });
     }
 
     // Close other accordions
     Array.from(el.parentNode.childNodes).forEach(sibling => {
-      if (sibling !== el) {
+      if (sibling !== el && sibling.classList.contains('open')) {
+        this.toggleTabIndex(sibling, false);
         sibling.classList.remove('open');
       }
     });
 
     // Analytics
     this.logEvent('skills', 'open', el.querySelector('.title').textContent);
+  }
+
+  // Toggles element tab index for accessibility
+  toggleTabIndex($el, bool) {
+    const index = bool ? 0 : -1;
+    const isCategory = $el.classList.contains('category');
+    let els = $el.querySelectorAll('[data-closable]');
+    if (isCategory && bool) {
+      // Only add tab index to skills
+      els = $el.querySelectorAll('article > section > [data-closable]');
+    } else if (isCategory) {
+      // Remove tab index from skills and buttons
+      els = $el.querySelectorAll('article > section [data-closable]');
+    } else {
+      // Only toggle the copy tab index
+      els = $el.querySelectorAll('article > section > article [data-closable]');
+    }
+    Array.from(els).forEach(el => {
+      el.tabIndex = index;
+    });
   }
 
   deeplink(id) {
