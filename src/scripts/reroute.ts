dbf9e3ff3b531603247b5e365cadd218ab3cc201
Brandon Pereira <brandonpereiras@gmail.com>
2023-05-22 00:28:12 -0600

    work to animations

--- src/scripts/reroute.ts
+++ src/scripts/reroute.ts
@@ -1,21 +1,48 @@
 navigation.addEventListener("navigate", (navigateEvent) => {
-  const url = new URL(navigateEvent.destination.url);
-
-  if (url.pathname.startsWith("/projects/")) {
+  const destination = new URL(navigateEvent.destination.url);
+  console.log("AAA");
+  if (destination.pathname.startsWith("/projects/")) {
+    const [, , id] = destination.pathname.split("/");
+    console.log(id);
     navigateEvent.intercept({
       async handler() {
-        const ProjectDetails = await fetch(
-          "/projects/fragments/ProjectDetails"
-        );
-        const text = await ProjectDetails.text();
-        const fragment = document.createRange().createContextualFragment(text);
-        // TODO:F allback
-        const transition = document.startViewTransition(() => {
-          document
-            .querySelector("[data-projects-content]")
-            ?.replaceWith(fragment);
-        });
+        try {
+          const ProjectDetails = await fetch(
+            "/projects/fragments/ProjectDetails"
+          );
+          const text = await ProjectDetails.text();
+          const fragment = document
+            .createRange()
+            .createContextualFragment(text);
+          const tile = document.querySelector<HTMLDivElement>(
+            `div[data-project-tile='${id}']`
+          )!;
+          tile.style.viewTransitionName = "project";
+
+          // @ts-expect-error
+          const transition = document.startViewTransition(() => {
+            try {
+              console.log("HI");
+              tile.style.viewTransitionName = "";
+
+              document
+                .querySelector("[data-projects-content]")
+                ?.replaceWith(fragment);
+              console.log("BYE");
+            } catch (err) {
+              console.error(err);
+            }
+          });
+
+          return undefined;
+        } catch (err) {
+          console.error(err);
+        }
       },
     });
   }
+});
+
+navigation.addEventListener("navigateerror", (navigateEvent) => {
+  console.log("HEre");
 });
