85172cfab545d2983461d5bb6bac4c17078f48c9
Brandon Pereira <brandonpereiras@gmail.com>
2023-07-14 15:43:26 -0600

    custom media css support

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,9 @@
 module.exports = {
-  plugins: [require("postcss-nesting")],
+  plugins: [
+    require("@csstools/postcss-global-data")({
+      files: ["./src/css/customMedia.css"],
+    }),
+    require("postcss-custom-media"),
+    require("postcss-nesting"),
+  ],
 };
