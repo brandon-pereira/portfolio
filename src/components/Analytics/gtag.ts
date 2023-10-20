3610cfa882496186597fa7969ab043f4343a99de
Brandon Pereira <brandonpereiras@gmail.com>
2023-10-19 18:09:01 -0600

    fix google analytics

---
+++ src/components/Analytics/gtag.ts
@@ -0,0 +1,10 @@
+const isDev = import.meta.env.DEV;
+
+if (!isDev) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: any[]) {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-KPH6GZV558');
+}
