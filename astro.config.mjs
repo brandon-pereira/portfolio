a8d5d21ce4a1dea835dcda3ed807a50a8f23d32c
Brandon Pereira <brandonpereiras@gmail.com>
2024-04-18 20:34:46 -0600

    bump dependencies, make external links open in a new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import serviceWorker from 'astrojs-service-worker';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
-  integrations: [serviceWorker()]
+  integrations: [serviceWorker()],
+  markdown: {
+    // forces external links to open in a new tab
+    rehypePlugins: [[rehypeExternalLinks, { target: '_blank' }]]
+  }
 });
