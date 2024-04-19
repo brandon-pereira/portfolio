948c6534220fdc8cdf082d89c0424178b25b184e
Brandon Pereira <brandonpereiras@gmail.com>
2024-04-19 16:53:54 -0600

    fix canonical urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import serviceWorker from 'astrojs-service-worker';
 import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
+  site: 'https://branclon.com',
   integrations: [serviceWorker()],
   markdown: {
     // forces external links to open in a new tab
