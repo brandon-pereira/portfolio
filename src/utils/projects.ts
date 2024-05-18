bd25494e8f65ca180c4ea37a5baf42f28dbe5647
Brandon Pereira <brandonpereiras@gmail.com>
2024-05-18 16:34:50 -0600

    sort logic changes

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,13 +1,22 @@
+import type { CollectionEntry } from 'astro:content';
 import { getCollection } from 'astro:content';
 
-export async function getSortedProjects() {
-  return (await getCollection('projects')).sort((a, b) => {
-    if (a.data.isPinned && !b.data.isPinned) {
-      return -1;
-    }
-    if (b.data.isPinned && !a.data.isPinned) {
-      return 1;
-    }
-    return b.data.date.getTime() < a.data.date.getTime() ? -1 : 1;
-  });
+type Project = CollectionEntry<'projects'>;
+
+export async function getProjects() {
+  return Array.from(await getCollection('projects'));
+}
+
+export function byPinned(a: Project, b: Project) {
+  if (a.data.isPinned && !b.data.isPinned) {
+    return -1;
+  }
+  if (b.data.isPinned && !a.data.isPinned) {
+    return 1;
+  }
+  return 0;
+}
+
+export function byCreationDate(a: Project, b: Project) {
+  return b.data.date.getTime() < a.data.date.getTime() ? -1 : 1;
 }
